9b7a8d12b49e59b29f48869c78664ca811476437
Chao Lobi <gogoiladitya@gmail.com>
2024-02-02 22:59:54 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+//  Pages
+import Login from './pages/Login';
+
+// styles
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <div className="routes">
+        <Routes>
+          <Route  exact path="/" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
