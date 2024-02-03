92f39d89361881cce3c23602c9c44572b5f6a670
Chao Lobi <gogoiladitya@gmail.com>
2024-02-03 20:06:03 +0530

    quick response team

--- src/components/ImergencyResponse/ImergencyResponse.jsx
+++ src/components/ImergencyResponse/ImergencyResponse.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+// assets
+import { TiPinOutline } from "react-icons/ti";
+
+// import stylesheet
+import classes from './ImergencyResponse.module.css';
+
 const ImergencyResponse = () => {
   return (
-    <div>ImergencyResponse</div>
+    <div className={classes.noticeContainer}>
+      <div className={classes.topbar}>
+        <div className={classes.name}>.Imergency Request.</div>
+      </div>
+      <div className={classes.noticeBox}>
+        <TiPinOutline className={classes.logo} />
+        <div className={classes.Category}>Theft</div>
+        <div className={classes.Location}>Food Court</div>
+        <button>check</button>
+      </div>
+    </div>
   )
 }
 
