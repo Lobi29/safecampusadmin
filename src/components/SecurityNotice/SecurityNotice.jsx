777e24839028f57bcc1738e5416b22bd0cd975c1
Chao Lobi <gogoiladitya@gmail.com>
2024-02-03 19:56:55 +0530

    notice board done

--- src/components/SecurityNotice/SecurityNotice.jsx
+++ src/components/SecurityNotice/SecurityNotice.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+// assets
+import { TiPinOutline } from "react-icons/ti";
+
+// import stylesheet
+import classes from './SecurityNotice.module.css';
+import MakeNotice from '../MakeNotice/MakeNotice';
+
 const SecurityNotice = () => {
   return (
-    <div>SecurityNotice</div>
+    <div className={classes.noticeContainer}>
+      <div className={classes.left}>
+        <div className={classes.topbar}>
+          <div className={classes.name}>.Notice Board.</div>
+        </div>
+        <div className={classes.noticeBox}>
+          <TiPinOutline className={classes.logo} />
+          <div className={classes.heading}>Don't Use K.V. road Today</div>
+          <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas aspernatur tempore, corrupti a iste deserunt vitae autem eligendi mollitia.</div>
+          <button>5/2/2024</button>
+        </div>
+        <div className={classes.noticeBox}>
+          <TiPinOutline className={classes.logo} />
+          <div className={classes.heading}>Don't Use K.V. road Today</div>
+          <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas aspernatur tempore, corrupti a iste deserunt vitae autem eligendi mollitia.</div>
+          <button>5/2/2024</button>
+        </div>
+        <div className={classes.noticeBox}>
+          <TiPinOutline className={classes.logo} />
+          <div className={classes.heading}>Don't Use K.V. road Today</div>
+          <div className={classes.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptas aspernatur tempore, corrupti a iste deserunt vitae autem eligendi mollitia.</div>
+          <button>5/2/2024</button>
+        </div>
+      </div>
+      <div className={classes.right}>
+        <MakeNotice />
+      </div>
+    </div>
   )
 }
 
